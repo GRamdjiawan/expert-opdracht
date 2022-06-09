0c6d2fe5e2a0e488bd627484b6fca3f260a5466a
Gianni <302325196@student.rocmondriaan.nl>
2022-06-09 15:09:55 +0200

    onchange toegevoegd

--- js/opdracht3.js
+++ js/opdracht3.js
@@ -1,20 +1,24 @@
 const btn = document.querySelector('.btn');
 const img = document.querySelector('.showImg');
 const show = document.querySelector('.hide');
+const passwordInput = document.querySelector('.password');
 
-
-btn.addEventListener('click', () => {
+function inputValue() {
     const password = document.querySelector('.password').value;
-
+    
     if(password.toLowerCase() == 'h@ck3d') {
         show.classList.add('show')
-
+    
         img.setAttribute('src', '../img/hacking.gif')
     } else {
         show.classList.add('show')
-
+    
         img.setAttribute('src', '../img/failed.gif')
     }
 
-    
-})
+}
+
+
+btn.addEventListener('click', inputValue)
+
+passwordInput.addEventListener('change',inputValue)
